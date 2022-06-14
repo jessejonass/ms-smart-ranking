29fd0ac25d04df5809da7365715e26530d516c11
Jesse Jonas <jessejonas13@gmail.com>
2022-06-13 22:02:41 -0300

    feat: added module create category service and controller

--- ms-admin/src/app.service.ts
+++ ms-admin/src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './categories/entities/Category';
+import { Player } from './players/entities/Player';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('Category') private readonly categoryModel: Model<Category>,
+    @InjectModel('Player') private readonly playerModel: Model<Player>,
+  ) {}
+
+  private readonly logger = new Logger(AppService.name);
+
+  async createCategory(category: Category): Promise<Category> {
+    try {
+      const newCategory = new this.categoryModel(category);
+      return await newCategory.save();
+    } catch (err) {
+      this.logger.error(`error: ${JSON.stringify(err.message)}`);
+      throw new RpcException(err.message);
+    }
   }
 }
