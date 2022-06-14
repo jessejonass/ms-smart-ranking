63201ff3698dfe04e8a65dedb9b9455b463ad083
Jesse Jonas <jessejonas13@gmail.com>
2022-06-13 21:05:01 -0300

    feat: added createCategory emitter

--- ms-api-gateway/src/app.controller.ts
+++ ms-api-gateway/src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
+  private clientAdminBackend: ClientProxy;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqp://user:o68wbnCiT1Yf@18.208.114.214:5672/api-smart-ranking',
+        ],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.clientAdminBackend.emit(
+      'create-category',
+      createCategoryDto,
+    );
   }
 }
