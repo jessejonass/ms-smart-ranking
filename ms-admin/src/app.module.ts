0d995d81e7973acda7dc65ab29f1a67e58ed4931
Jesse Jonas <jessejonas13@gmail.com>
2022-06-13 22:02:11 -0300

    feat: added module mongoose connections

--- ms-admin/src/app.module.ts
+++ ms-admin/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategorySchema } from './categories/entities/category.schema';
+import { PlayerSchema } from './players/entities/Player.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@nestjs-api-smart-rankin.l80zm.mongodb.net/api-smart-ranking-adm-backend?retryWrites=true&w=majority',
+    ),
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+    MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
