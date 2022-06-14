73eefee47c1d6c904657a435b3cf298a5faed354
Jesse Jonas <jessejonas13@gmail.com>
2022-06-14 10:40:50 -0300

    feat: added timeout interceptor

--- ms-api-gateway/src/main.ts
+++ ms-api-gateway/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { LogginInterceptor } from './common/interceptors/logging.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalInterceptors(new LogginInterceptor());
+  app.useGlobalInterceptors(new LogginInterceptor(), new TimeoutInterceptor());
 
   Date.prototype.toJSON = function (): any {
     return momentTimezone(this)
