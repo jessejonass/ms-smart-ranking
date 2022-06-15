1165ee5c182eff5b098c3f99d122d7c6d337beeb
Jesse Jonas <jessejonas13@gmail.com>
2022-06-15 17:34:36 -0300

    feat: updated proxy rmq

--- ms-challenges/src/proxy/client-proxy.ts
+++ ms-challenges/src/proxy/client-proxy.ts
@@ -24,4 +24,19 @@ export class ClientProxySmartRanking {
       },
     });
   }
+
+  getClientProxyChallengeInstance(): ClientProxy {
+    const RABBITMQ_USER = this.configService.get<string>('RABBITMQ_USER');
+    const RABBITMQ_PASSWORD =
+      this.configService.get<string>('RABBITMQ_PASSWORD');
+    const RABBITMQ_URL = this.configService.get<string>('RABBITMQ_URL');
+
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RABBITMQ_URL}`],
+        queue: 'challenges',
+      },
+    });
+  }
 }
