2c8beaa3c778acc258443269cbc92afa973965cd
Jesse Jonas <jessejonas13@gmail.com>
2022-06-15 11:54:04 -0300

    feat: added aws module, service and import in players module

--- ms-api-gateway/src/players/players.module.ts
+++ ms-api-gateway/src/players/players.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AwsModule } from 'src/aws/aws.module';
 import { ProxyModule } from 'src/proxy/proxy.module';
 import { PlayersController } from './players.controller';
 
 @Module({
-  imports: [ProxyModule],
+  imports: [ProxyModule, AwsModule],
   controllers: [PlayersController],
 })
 export class PlayersModule {}
