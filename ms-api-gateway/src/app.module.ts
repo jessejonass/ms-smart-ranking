099318ce5242dd9379a4fd08c4479d40e9b5435a
Jesse Jonas <jessejonas13@gmail.com>
2022-06-15 11:53:11 -0300

    config: added config module to use .env

--- ms-api-gateway/src/app.module.ts
+++ ms-api-gateway/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
 import { PlayersModule } from './players/players.module';
 import { ProxyModule } from './proxy/proxy.module';
+import { AwsModule } from './aws/aws.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CategoriesModule, PlayersModule, ProxyModule],
+  imports: [
+    AwsModule,
+    CategoriesModule,
+    PlayersModule,
+    ProxyModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
