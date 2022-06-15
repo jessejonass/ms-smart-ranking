623ff7bac8f87906e078a87a0afa8f25d13a155f
Jesse Jonas <jessejonas13@gmail.com>
2022-06-15 13:31:43 -0300

    feat: added proxy module

--- ms-challenges/src/app.module.ts
+++ ms-challenges/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ChallengesModule } from './challenges/challenges.module';
 import { MatchesModule } from './matches/matches.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProxyModule } from './proxy/proxy.module';
 
 @Module({
   imports: [
     ChallengesModule,
     MatchesModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ProxyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
