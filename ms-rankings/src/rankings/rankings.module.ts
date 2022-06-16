94bdfe1a5c21a715c6e6df00ec15a8fb4ee86c39
Jesse Jonas <jessejonas13@gmail.com>
2022-06-16 13:56:19 -0300

    feat: added ranking schema

--- ms-rankings/src/rankings/rankings.module.ts
+++ ms-rankings/src/rankings/rankings.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RankingsService } from './rankings.service';
 import { RankingsController } from './rankings.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RankingSchema } from './entities/Ranking.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }]),
+  ],
   providers: [RankingsService],
-  controllers: [RankingsController]
+  controllers: [RankingsController],
 })
 export class RankingsModule {}
