0f21e3f424629efe3765764bc0bb58a533cdcbf8
Jesse Jonas <jessejonas13@gmail.com>
2022-06-17 10:18:27 -0300

    chore: removed moment-timezone - added at microservices

--- ms-api-gateway/src/main.ts
+++ ms-api-gateway/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as momentTimezone from 'moment-timezone';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { LogginInterceptor } from './common/interceptors/logging.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout-interceptor';
@@ -9,12 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LogginInterceptor(), new TimeoutInterceptor());
-
-  Date.prototype.toJSON = function (): any {
-    return momentTimezone(this)
-      .tz('America/Sao_Paulo')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
-  };
 
   await app.listen(8080);
 }
