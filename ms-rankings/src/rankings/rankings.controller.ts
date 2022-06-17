f81dc2aa78364bb687efa4e6aee5213ce775e864
Jesse Jonas <jessejonas13@gmail.com>
2022-06-17 15:38:00 -0300

    feat: added rankings service and controller

--- ms-rankings/src/rankings/rankings.controller.ts
+++ ms-rankings/src/rankings/rankings.controller.ts
@@ -1,4 +1,58 @@
 import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { Match } from './entities/Match';
+import { RankingResponse } from './entities/RankingResponse';
+import { RankingsService } from './rankings.service';
 
-@Controller('rankings')
-export class RankingsController {}
+const ackErrors = ['E11000'];
+
+@Controller()
+export class RankingsController {
+  constructor(private readonly rankingsService: RankingsService) {}
+
+  @EventPattern('proccess-match')
+  async proccessMatch(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      const matchId: string = data.matchId;
+      const match: Match = data.match;
+
+      await this.rankingsService.proccessMatch(matchId, match);
+
+      await channel.ack(originalMessage);
+    } catch (err) {
+      const filterAckError = ackErrors.filter((ackError) =>
+        err.message.includes(ackError),
+      );
+
+      if (filterAckError.length > 0) {
+        await channel.ack(originalMessage);
+      }
+    }
+  }
+
+  @MessagePattern('get-rankings')
+  async find(
+    @Payload() data: any,
+    @Ctx() context: RmqContext,
+  ): Promise<RankingResponse[] | RankingResponse> {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      const { categoryId, dateRef } = data;
+
+      return await this.rankingsService.find(categoryId, dateRef);
+    } finally {
+      await channel.ack(originalMessage);
+    }
+  }
+}
