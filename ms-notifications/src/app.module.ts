f4cb1800a73db2cfe2daa49e99eb9f3b9e4ad965
Jesse Jonas <jessejonas13@gmail.com>
2022-06-17 17:27:50 -0300

    feat: added smt configs ms-notifications

--- ms-notifications/src/app.module.ts
+++ ms-notifications/src/app.module.ts
@@ -1,3 +1,4 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
@@ -5,7 +6,24 @@ import { AppService } from './app.service';
 import { ProxyModule } from './proxy/proxy.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ProxyModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MailerModule.forRoot({
+      transport: {
+        host: 'email-smtp.us-east-1.amazonaws.com',
+        port: 587,
+        secure: false,
+        tls: {
+          ciphers: 'SSLv3',
+        },
+        auth: {
+          user: 'AKIATO4MWY7Y55OE5JTZ',
+          pass: 'BGxq4uRNuM/n0mdmYJz6fdbBWtHnLsHNv8AaRwkUQVv1',
+        },
+      },
+    }),
+    ProxyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
