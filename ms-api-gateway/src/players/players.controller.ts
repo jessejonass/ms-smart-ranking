d324859d12f134078c32d07d26ad62d1d5a41227
Jesse Jonas <jessejonas13@gmail.com>
2022-06-17 18:26:46 -0300

    refactor: refactored players module, services and controllers ms-api-gateway

--- ms-api-gateway/src/players/players.controller.ts
+++ ms-api-gateway/src/players/players.controller.ts
@@ -1,11 +1,8 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
-  Logger,
-  NotFoundException,
   Param,
   Post,
   Put,
@@ -16,67 +13,33 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { lastValueFrom, Observable } from 'rxjs';
-import { AwsService } from 'src/aws/aws.service';
 import { ValidationParamsPipe } from 'src/common/pipes/validation-params.pipe';
-import { ClientProxySmartRanking } from 'src/proxy/client-proxy';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { UpdatePlayerDto } from './dtos/update-player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
-  constructor(
-    private clientProxySmartRanking: ClientProxySmartRanking,
-    private awsService: AwsService,
-  ) {}
-
-  private logger = new Logger(PlayersController.name);
-
-  private clientAdminBackend =
-    this.clientProxySmartRanking.getClientProxyAdminBackendInstance();
+  constructor(private playerService: PlayersService) {}
 
   @Post()
   @UsePipes(ValidationPipe)
   async create(@Body() createPlayerDto: CreatePlayerDto): Promise<void> {
-    // check category exists sending a event emitter with rmq
-    const category = await lastValueFrom(
-      this.clientAdminBackend.send('get-categories', createPlayerDto.category),
-    );
-
-    if (category) {
-      this.clientAdminBackend.emit('create-player', createPlayerDto);
-    } else {
-      throw new NotFoundException('Category not found');
-    }
+    return await this.playerService.create(createPlayerDto);
   }
 
   @Post(':_id/upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file, @Param('_id') _id: string) {
-    const playerExists = await this.clientAdminBackend.send('get-players', _id);
-
-    if (!playerExists) {
-      throw new BadRequestException('Player not found');
-    }
-
-    const imageUrl = await this.awsService.uploadFile(file, _id);
-
-    const updatePlayerDto: UpdatePlayerDto = {};
-    updatePlayerDto.imageUrl = imageUrl.url;
-
-    await lastValueFrom(
-      this.clientAdminBackend.emit('update-player', {
-        _id: _id,
-        player: updatePlayerDto,
-      }),
-    );
-
-    return this.clientAdminBackend.send('get-players', _id);
+  async uploadFile(
+    @UploadedFile() file: any,
+    @Param('_id') _id: string,
+  ): Promise<any> {
+    return await this.playerService.uploadFile(file, _id);
   }
 
   @Get()
-  find(@Query('_id') _id: string): Observable<any> {
-    return this.clientAdminBackend.send('get-players', _id ? _id : '');
+  async find(@Query('_id') _id: string): Promise<any> {
+    return await this.playerService.find(_id);
   }
 
   @Put(':_id')
@@ -85,23 +48,11 @@ export class PlayersController {
     @Param('_id') _id: string,
     @Body() updatePlayerDto: UpdatePlayerDto,
   ): Promise<void> {
-    // check category exists sending a event emitter with rmq
-    const category = await lastValueFrom(
-      this.clientAdminBackend.send('get-categories', updatePlayerDto.category),
-    );
-
-    if (category) {
-      this.clientAdminBackend.emit('update-player', {
-        _id,
-        player: updatePlayerDto,
-      });
-    } else {
-      throw new NotFoundException('Category not found');
-    }
+    return await this.playerService.update(_id, updatePlayerDto);
   }
 
   @Delete(':_id')
   async delete(@Param('_id', ValidationParamsPipe) _id: string): Promise<void> {
-    this.clientAdminBackend.emit('delete-player', _id);
+    return await this.playerService.delete(_id);
   }
 }
--- ms-api-gateway/src/players/players.module.ts
+++ ms-api-gateway/src/players/players.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AwsModule } from 'src/aws/aws.module';
 import { ProxyModule } from 'src/proxy/proxy.module';
 import { PlayersController } from './players.controller';
+import { PlayersService } from './players.service';
 
 @Module({
   imports: [ProxyModule, AwsModule],
   controllers: [PlayersController],
+  providers: [PlayersService],
 })
 export class PlayersModule {}
